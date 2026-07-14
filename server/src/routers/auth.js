f6fb63d2e1df6cc5e2efe06287a5737a56767f39
Cara Qing Ge <geqing2012@hotmail.com>
2026-07-13 21:14:37 -0400

    feat: implement signup with bcrypt and JWT

--- server/src/routers/auth.js
+++ server/src/routers/auth.js
@@ -1,8 +1,57 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
+const User = require("../models/User");
+// const CustomAPIError = require("../errors");
 const router = express.Router();
 
-router.post("/signup", async (req, res) => {
-  res.json({ message: "signup endpoint" });
+router.post("/signup", async (req, res, next) => {
+  try {
+    const { email, password, isVendor } = req.body;
+    //validate email and password
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({ message: "Invalid email!" });
+    }
+
+    if (!password || password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters!" });
+    }
+    //check whether the user is alreay signed up
+    let user = await User.findOne({ email });
+    if (user) {
+      return res
+        .status(409)
+        .json({ message: "The email has been registered!" });
+    }
+
+    //hash password
+    const hashed = await bcrypt.hash(password, 10);
+
+    //store db
+    user = await User.create({
+      email,
+      password: hashed,
+      role: isVendor ? "vendor" : "regular",
+    });
+
+    const payload = {
+      sub: user._id,
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
+    res.status(201).json({
+      token,
+      user: { id: user._id, email: user.email, role: user.role },
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
