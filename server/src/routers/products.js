4cd6547128a79b2c04a7981406133bbc9056f555
Cherish <cherishkangwj@gmail.com>
2026-07-15 08:07:00 -0700

    feat: add product edit/delete APIs with vendor role check

--- server/src/routers/products.js
+++ server/src/routers/products.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { requireVendorRole } = require("../middlewares/auth");
+const { requireJwt, requireVendorRole } = require("../middlewares/auth");
 const Product = require("../models/Product");
 const DEFAULT_LIMIT = 10;
 
-//get product list(pagination, sorting, search)
+//get product list(pagination, sorting, search(bonus))
 router.get('/', async(req, res) => {
     try{
         const { page = 1, sortBy = "createdAt", sortOrder = "desc" } = req.query;
@@ -34,27 +34,73 @@ router.get('/', async(req, res) => {
 });
 
 //get product description
-router.get('/:id', (req, res) => {
-    res.json({message: "detail placeholder", id: req.params.id});
+router.get('/:id', async (req, res) => {
+    try{
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        if(!product){
+            return res.status(404).json({message: "Product not found"});
+        }
+        res.status(200).json(product);
+    }
+    catch(err){
+        res.status(500).json({message: err.message});
+    }
 });
 
 
 //create products
-router.post('/', requireVendorRole, async(req, res) => {
-    res.json({message: "detail placeholder", id: req.params.id});
+router.post('/', requireJwt, requireVendorRole, async(req, res) => {
+    try{
+        const{ name, price, imageUrl, description="", stock, category="books"} = req.body;
+        const newProduct = new Product({
+            name: name,
+            price: price,
+            imageUrl: imageUrl,
+            description: description,
+            stock: stock,
+            category: category,
+        }) 
+        const savedProduct = await newProduct.save();
+        res.status(201).json(savedProduct);
+    }
+    catch(err){
+        res.status(500).json({err: err.message});
+    }
 
 });
 
 
 //edit products
-router.put('/:id', requireVendorRole, async(req, res) => {
-    res.json({message: "detail placeholder", id: req.params.id});
-
+router.put('/:id', requireJwt, requireVendorRole, async(req, res) => {
+    try{
+        const updatedProduct = await Product.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        if(!updatedProduct){
+            return res.status(404).json({message: "Product cannot be found"});
+        }
+        res.status(200).json(updatedProduct);
+    }
+    catch(err){
+        res.status(500).json({message: "err.message"});
+    }
 });
 
 //delete products
-router.delete('/:id', requireVendorRole, async(req, res) => {
-    res.json({message: "detail placeholder", id: req.params.id});
+router.delete('/:id', requireJwt, requireVendorRole, async(req, res) => {
+    try{
+        const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+        if(!deletedProduct){
+            return res.status(404).json({message: "Product not found"});
+        }
+        res.status(200).json({message: "Product deleted!", data: deletedProduct});
+    }
+    catch(err){
+        res.status(500).json({message: err.message});
+    }
 });
 
 
