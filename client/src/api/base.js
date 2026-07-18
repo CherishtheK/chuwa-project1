c5cc514cfdf9e5fe3e379138129854bf3f01e593
Cherish <cherishkangwj@gmail.com>
2026-07-18 14:05:48 -0700

    chore: regenerate package-lock, finalize api/base.js after merge

--- client/src/api/base.js
+++ client/src/api/base.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 import axios from "axios";
-
 const api = axios.create({
   baseURL: "/api",
   headers: { "Content-Type": "application/json" },
 });
-
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
@@ -13,19 +10,4 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
-
 export default api;
-=======
-import axios from 'axios';
-
-const API_BASE_URL = '/api';
-
-const api = axios.create({
-    baseURL: API_BASE_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    }
-})
-
-export default api;
->>>>>>> 04411bb (feat: add product api functions (get/create/edit/delete), tested against backend)
