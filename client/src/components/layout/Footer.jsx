937fae407c49b1f8cae4115cb48ae316c6bba4ed
Cherish <cherishkangwj@gmail.com>
2026-07-18 11:48:11 -0700

    feat: implement header and footer with responsive layout

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -1,5 +1,25 @@
+import { YoutubeOutlined, TwitterCircleFilled, FacebookOutlined } from "@ant-design/icons";
+
 function Footer() {
-  return <div>©2026 All Rights Reserved.</div>;
+  return(
+    <footer className="flex items-center justify-between px-8 py-6 bg-black text-white text-sm">
+      <div>©2026 All Rights Reserved.</div>  
+ 
+      <div className="flex gap-4 text-xl">
+        <YoutubeOutlined />
+        <TwitterCircleFilled />
+        <FacebookOutlined />
+      </div>
+
+      <div className=" flex gap-6">
+          <span>Contact us</span>
+          <span>Privacy Policies</span>
+          <span>Help</span>
+      </div>
+
+
+    </footer>
+  ) 
 }
 
 export default Footer;
--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,5 +1,34 @@
+import { Input } from "antd";
+import { ShoppingCartOutlined } from "@ant-design/icons"
+import { Badge, Button } from 'antd';
+import { useSelector } from "react-redux";
+
 function Header() {
-  return <div>Header</div>;
+  const user = useSelector(state => state.auth.user)
+  return (
+    <header className="flex items-center justify-between px-8 py-4 bg-black text-white">
+      <div className="mr-8 text-2xl font-bold">
+        Management <span className="text-sm">Chuwa</span>
+      </div>
+
+      
+      <div className="flex-1 flex items-center">
+        <Input.Search placeholder="Search" className="!w-96" />
+      </div>
+
+
+      <div className="flex items-center gap-6">
+        <Button type="text" className="!text-white text-lg">
+          {user? "Sign Out" : "Sign In"}
+        </Button>
+        <Badge count={2}>
+          <ShoppingCartOutlined className="!text-2xl !text-white" />
+        </Badge>
+        <span>$price</span>
+      </div>
+
+    </header>
+  );
 }
 
 export default Header;
