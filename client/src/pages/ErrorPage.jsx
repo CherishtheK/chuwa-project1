b6a0d88cf50e556d0ab7ccfd24b454a8594c789a
Cara Qing Ge <geqing2012@hotmail.com>
2026-07-20 12:52:21 -0400

    feat:completed error page

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,5 +1,20 @@
+import { Result, Button } from "antd";
+
+import { useNavigate } from "react-router-dom";
+
 function ErrorPage() {
-  return <div>error</div>;
+  const navigate = useNavigate();
+  return (
+    <Result
+      status="404"
+      subTitle={<h1>Oops, something went wrong!</h1>}
+      extra={
+        <Button type="primary" onClick={() => navigate("/")}>
+          Back Home
+        </Button>
+      }
+    />
+  );
 }
 
 export default ErrorPage;
